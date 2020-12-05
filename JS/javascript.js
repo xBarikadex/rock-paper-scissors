c9a20d58f697a6689a960add8ee80db5fc3f67e5
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2020-12-04 18:21:44 -0700

    Day 1

---
+++ JS/javascript.js
@@ -0,0 +1,10 @@
+let playerInput = 0;
+let computerInput = Math.ceil(Math.random() * 3));
+
+function rock() {playerInput = 1;}
+function paper() {playerInput = 2;}
+function scissors() {playerInput = 3;}
+
+if (playerInput == computerInput) {
+    console.log("")
+}
